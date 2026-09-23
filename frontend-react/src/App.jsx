bd9b936d8cdd951560af3e47f103f9a6a490e88a
sairam <saiyadav79894@gmail.com>
2026-09-23 12:49:00 +0530

    homepage

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import './assets/css/style.css'
 import './App.css'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
